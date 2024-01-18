a206f06dc65a7b88666d2b294682305dd455191f
LuisCorreaDv <dev.luis.correa@gmail.com>
2024-01-17 18:25:58 -0600

    Estructura del ejercicio y obteniendo la lista de botones

---
+++ Counter/app.js
@@ -0,0 +1,8 @@
+//Contador inicial 
+let count = 0;
+
+//seleccionar el valor y los botones
+const vlaue = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
