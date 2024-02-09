ccff95008247239fec502f2f4f7b731f4bb0648b
LuisCorreaDv <dev.luis.correa@gmail.com>
2024-02-09 14:57:43 -0600

    Toggle para agregar la clase y mostrar/ocultar texto de las preguntas

--- Questions/app.js
+++ Questions/app.js
@@ -2,6 +2,8 @@ const btns = document.querySelectorAll('.question-btn');
 
 btns.forEach((btn) => {
     btn.addEventListener("click", (e)=> {
-        console.log(e.currentTarget.parentElement);
+        // console.log(e.currentTarget.parentElement.parentElement);
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text');
     })
 });
